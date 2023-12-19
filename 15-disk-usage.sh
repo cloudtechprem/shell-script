15b4f8b4bf065f40bec81ea17222138d216f0967
cloudtechprem <cloudtechprem@gmail.com>
2023-12-19 21:34:27 +0530

    2nd commit

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -2,13 +2,13 @@
 
 # We are trying to a get the disk usage if the threshold exceeds 1%
 
-DISK_USAGE=$(df -hT | grep vE 'tmp|File') # we dont want tmp and File in the output so we are using vE 'tmp|File : | - denotes "AND" here'
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File') # we dont want tmp and File in the output so we are using vE 'tmp|File : | - denotes "AND" here'
 DISK_THRESHOLD=1
 message=""
 
 while IFS= read line
 do
-    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1) # using awk command to get the 6th fragment in grep vE 'tmp|File' and cut command to remove the percentage
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1) # using awk command to get the 6th fragment in grep -vE 'tmp|File' and cut command to remove the percentage
     partition=$(echo $line awk '{print $1F}')
     if [ $usage -gt $DISK_USAGE ]
     then 
